d3b0f236b72c742aaff810f415f8f2dc97aba8c0
Mark Murphy <boathouse2112@gmail.com>
2023-05-04 17:17:25 -0400

    Bootable kernel

---
+++ README.md
@@ -0,0 +1,5 @@
+# xv6 x86_64 Rust
+
+Current state:
+- Compile standalone rust executable -- No libc, no std
+
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,23 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // Memory location of VGA mode buffer
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    // Write to the VGA mode buffer
+    // Lower byte is character ASCII code-point
+    // Higher (second?) byte is color data
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
