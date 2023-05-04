e41403c5547ab45c4991ab03a481f42e01294d1d
Mark Murphy <boathouse2112@gmail.com>
2023-05-04 19:27:11 -0400

    Add a VGA-mode buffer and println! macro. Implement panic

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 #![no_main]
 #![no_std]
 
-use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World";
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Memory location of VGA mode buffer
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    // Write to the VGA mode buffer
-    // Lower byte is character ASCII code-point
-    // Higher (second?) byte is color data
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("no way never{}", "!");
+    panic!("It broke");
 
     loop {}
 }
 
 #[panic_handler]
-fn panic(_panic: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
